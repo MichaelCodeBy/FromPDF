3218b9bf69af43b94c85781948adef8cc437647b
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2022-11-15 11:58:56 +0300

    start

---
+++ src/PdfToText.java
@@ -0,0 +1,13 @@
+
+import org.apache.pdfbox.*;
+import org.apache.pdfbox.pdmodel.*;
+//import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.*;
+
+
+public class PdfToText {
+    public static void main(String[] args) {
+
+    }
+}
